949dfb8669beaf053c96e54580f6f95f67331236
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2024-02-23 23:28:06 +0100

    Update GameAPI

--- API/GameAPI.py
+++ API/GameAPI.py
@@ -1,7 +1,6 @@
 import requests
 from dotenv import load_dotenv
 import os
-import json
 import webbrowser
 
 
@@ -11,16 +10,23 @@ rapid_key=os.getenv("RAPID_API_KEY")
 raw_key=os.getenv("RAWG_API_KEY")
 
 
-url = "https://rawg-video-games-database.p.rapidapi.com/games?key="+raw_key+"&tags=multiplayer"
+url = "https://rawg-video-games-database.p.rapidapi.com/games?key="+raw_key
 
 headers = {
 	"X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com",
     "X-RapidAPI-Key": rapid_key
 }
 
+
+print("Enter Gamen name: ")
+game=input()
+url+="&search_exact=true&search_precise=true&search="+game
+
+#print(url)
+
 response = requests.get(url, headers=headers)
 
 data=response.json()
 
-# webbrowser.open(data['results'][1]['background_image']) The witcher background opens in browser
-print(data['results'])
+webbrowser.open(data['results'][0]['background_image']) 
+#print(data['results'][1]['background_image'])
