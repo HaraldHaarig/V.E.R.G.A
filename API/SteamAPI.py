fb5de050c08f602dbf52154ae3d542fb37fba6c3
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2024-03-08 13:22:19 +0100

    Added testing Riot_API, Added folder Tkinter_GUI

--- API/SteamAPI.py
+++ API/SteamAPI.py
@@ -13,12 +13,17 @@ steam=Steam(KEY)
 print("Enter SteamID: ")
 id=input()
 
-user=steam.users.search_user(id)
+user=steam.users.get_user_details(id)
+print(user)
+# friendlist=steam.users.get_user_friends_list(user['player']['steamid'])
+# print(friendlist)
+
 
 response=steam.users.get_owned_games(user['player']['steamid'])
+print(response)
 count=response['game_count']
 
 for i in range(count):
-    print(response['games'][i]['name'])
-    openWebGame(response['games'][i]['name'])
+     print(response['games'][i]['name'])
+     openWebGame(response['games'][i]['name'])
     
---
+++ API/UsesRiotAPI.py
@@ -0,0 +1,4 @@
+from RiotAPI import get_summoner_info
+
+summoner=get_summoner_info("ErkanHartwixen","urmum")
+print(summoner)
