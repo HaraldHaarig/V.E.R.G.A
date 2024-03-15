8a49997700fa63a377e9842f22a066ffdf32dbb4
Je5usP46LO69420 <tobiashirzi@icloud.com>
2024-03-15 14:54:33 +0100

    startsite anordnung frames

--- Tkinter_GUI/Startseite.py
+++ Tkinter_GUI/Startseite.py
@@ -1,6 +1,10 @@
+import imp
 import customtkinter
-from PIL import Image
 import PIL
+from PIL import Image,ImageTk
+from tkinter import Canvas, PhotoImage, Tk
+from tkinter.ttk import Frame,Label,Style
+
 
 
 
@@ -24,9 +28,25 @@ for data in get_monitors():
     print(str(data))
     
 
-frame1 = customtkinter.CTkFrame(master=app, width=200, height=250,corner_radius=20,fg_color="#250454",bg_color="black")
-frame1.pack(side="left",expand="True")
+framesettings = customtkinter.CTkFrame(master=app, width=200, height=250,corner_radius=20, fg_color="#250454",bg_color="black")
+framesettings.place(x=250, y=70)
+#tkimg = PhotoImage(file="C:\Users\Tobias Hirzenberger\OneDrive\Desktop\3AHINF\SYP\V.E.R.G.A\Design\settings.png")
+#Canvas.create_image(0,0,anchor="nw",image=tkimg)
 
+framestore = customtkinter.CTkFrame(master=app, width=200, height=250,corner_radius=20, fg_color="#250454",bg_color="black")
+framestore.place(x=540,y=70)
+
+framemygames = customtkinter.CTkFrame(master=app, width=200, height=250,corner_radius=20, fg_color="#250454",bg_color="black")
+framemygames.place(x=830,y=70)
+
+framewishlist = customtkinter.CTkFrame(master=app, width=200, height=250,corner_radius=20, fg_color="#250454",bg_color="black")
+framewishlist.place(x=830, y=370)
+
+framenotes= customtkinter.CTkFrame(master=app, width=200, height=250,corner_radius=20, fg_color="#250454",bg_color="black")
+framenotes.place(x=540, y=370)
+
+framesupport = customtkinter.CTkFrame(master=app, width=200, height=250,corner_radius=20, fg_color="#250454",bg_color="black")
+framesupport.place(x=250,y=370)
 
 
 app.mainloop()
