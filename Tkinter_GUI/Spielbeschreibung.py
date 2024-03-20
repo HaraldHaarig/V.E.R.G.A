209649f08143749869547f03e9d51e563ed7baa6
binmarkoo <101064382+binmarkoo@users.noreply.github.com>
2024-03-20 09:26:04 +0100

    Added Labels to the Game Stats

--- Tkinter_GUI/Spielbeschreibung.py
+++ Tkinter_GUI/Spielbeschreibung.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import tkinter
 from tkinter.font import Font
 from turtle import color
@@ -39,7 +40,8 @@ pywinstyles.set_opacity(stats, color="#000001")
 stats.place(x=1005, y=450, anchor=tkinter.CENTER)
 
 # Label für die Game Beschreibung
-description_lbl = CTkLabel(app, text="Game Beschreibung", 
+description_lbl = CTkLabel(app, 
+                           text="Game Beschreibung", 
                            text_color="White", 
                            bg_color="#000001", 
                            font=("Arial", 25))
@@ -47,12 +49,49 @@ pywinstyles.set_opacity(description_lbl, color="#000001")
 description_lbl.place(x=425, y=275, anchor=tkinter.CENTER)
 
 # Label für die Game "Stats"
-stats_lbl = CTkLabel(app, text="Game Stats", 
+stats_lbl = CTkLabel(app, 
+                     text="Game Stats", 
                      text_color="White", 
                      bg_color="#000001", 
                      font=("Arial", 25))
 pywinstyles.set_opacity(stats_lbl, color="#000001")
 stats_lbl.place(x=1005, y=275, anchor=tkinter.CENTER)
+
+# Label für die Playtime
+playtime_lbl = CTkLabel(app, 
+                        text="Playtime: ",
+                        text_color="White",
+                        bg_color="#000001",
+                        font=("Arial", 22))
+pywinstyles.set_opacity(playtime_lbl, color="#000001")
+playtime_lbl.place(x=875, y=325, anchor=tkinter.CENTER)
+
+# Label für den Publisher
+publisher_lbl = CTkLabel(app, 
+                        text="Publisher: ",
+                        text_color="White",
+                        bg_color="#000001",
+                        font=("Arial", 22))
+pywinstyles.set_opacity(publisher_lbl, color="#000001")
+publisher_lbl.place(x=875, y=360, anchor=tkinter.CENTER)
+
+# Label für den Developer
+developer_lbl = CTkLabel(app, 
+                        text="Developer: ",
+                        text_color="White",
+                        bg_color="#000001",
+                        font=("Arial", 22))
+pywinstyles.set_opacity(developer_lbl, color="#000001")
+developer_lbl.place(x=875, y=395, anchor=tkinter.CENTER)
+
+# Label für das Erscheinungsdatum
+releasedate_lbl = CTkLabel(app, 
+                        text="Release Date: ",
+                        text_color="White",
+                        bg_color="#000001",
+                        font=("Arial", 22))
+pywinstyles.set_opacity(releasedate_lbl, color="#000001")
+releasedate_lbl.place(x=875, y=430, anchor=tkinter.CENTER)
 
 # app.overrideredirect(True)            Remove Titlebar (mehr oder weniger)
 app.mainloop()
