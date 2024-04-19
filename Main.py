6ccc4e2e9db89e59da8ec51b9b03e2b59a46142e
HaraldHaarig <132935818+HaraldHaarig@users.noreply.github.com>
2024-04-19 14:54:41 +0200

    Library started fr

--- Main.py
+++ Main.py
@@ -1,25 +1,25 @@
-import imp
-from Tkinter_GUI.Spielcards import Spielcards
-from Tkinter_GUI.Startseite import Startpage
-from API.SteamAPI import getSteamGamesbyID
+from Tkinter_GUI.Bibliothek import Bibliothek
+
 # Test_SteamID:76561199015522225 -> Sonrotz0815
 
 
 # generate classes
-test = Spielcards()
+bibliothek = Bibliothek()
 
-startpage = Startpage()
+# test = Spielcards()
+
+# startpage = Startpage()
 
 
 
 
-url,title=getSteamGamesbyID("76561199015522225")
+# url,title=getSteamGamesbyID("76561199015522225")
 
-# print(len(url), len(title) )
-count=0
-for s in url:
-    if s == 0:
-        url[count]="https://cdn-icons-png.flaticon.com/512/16/16096.png"
-    count+=1
-test.showallCards(url,title)
+# # print(len(url), len(title) )
+# count=0
+# for s in url:
+#     if s == 0:
+#         url[count]="https://cdn-icons-png.flaticon.com/512/16/16096.png"
+#     count+=1
+# test.showallCards(url,title)
 # test.showCard(url[0],title[0])
--- Tkinter_GUI/Bibliothek.py
+++ Tkinter_GUI/Bibliothek.py
@@ -1,13 +1,50 @@
 from customtkinter import* 
+#from tkinter import*
+import tkinter as tk
 from PIL import Image
+from ctypes import windll
+import ctypes
 
-app = CTk()
-app.geometry("1280x720")
-app.resizable(0,0)
+class Bibliothek:
 
-background = Image.open("Tkinter_GUI/Images/Background_frfr.png")
-backgroundData = CTkImage(dark_image=background, light_image=background, size=(1280, 720))
+    def __init__(self):
+        self.root_tk = CTk()
+        user32 = ctypes.windll.user32
+        screenwidth = user32.GetSystemMetrics(0)
+        screenheight=user32.GetSystemMetrics(1)
+        
+        x = int(((screenwidth/40)))
+        y = int(((screenheight/40)))
 
-CTkLabel(master=app, text="", image=backgroundData).pack(expand=True, side="left")
+        self.root_tk.geometry("%dx%d+%d+%d" % (int(screenwidth),int(screenheight),int(x),int(y)))
+        
+        self.root_tk.title("Bibliothek")
 
-app.mainloop()
+        #Pictures
+        leftside_img_data = Image.open("Tkinter_GUI/Images/Background_frfr.png")
+        background_img_data = Image.open("Tkinter_GUI/Images/Background_frfr.png")
+        #hier weiter machen
+
+        #Right side of the libary
+        self.mainframe= CTkFrame(self.root_tk, width=screenwidth,height=screenheight, fg_color="#222234") 
+        #self.mainframe.pack()
+        self.mainframe.place(anchor=tk.E, relx=1.0,rely=0.5)
+
+        #Left side of the library
+        self.leftframe= CTkFrame(self.root_tk, width=screenwidth-1620,height=screenheight, fg_color="#ffffff") 
+        #self.leftframe.pack()
+        self.leftframe.place(anchor=tk.W, relx=0.0,rely=0.5)
+
+        CTkLabel(master=self.leftframe, text="", image=side_img).pack(expand=True, side="left")
+
+        self.root_tk.mainloop()
+        # app = CTk()
+        # app.geometry("1280x720")
+        # app.resizable(0,0)
+
+        # background = Image.open("Tkinter_GUI/Images/Background_frfr.png")
+        # backgroundData = CTkImage(dark_image=background, light_image=background, size=(1280, 720))
+
+        # CTkLabel(master=app, text="", image=backgroundData).pack(expand=True, side="left")
+
+        # app.mainloop()
