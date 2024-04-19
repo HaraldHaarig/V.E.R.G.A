ef1e49be7def105f7125e260eed6ee9adc90a040
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2024-04-19 13:36:32 +0200

    Test commit changed main

--- Main.py
+++ Main.py
@@ -4,22 +4,19 @@ from Tkinter_GUI.Startseite import Startpage
 from API.SteamAPI import getSteamGamesbyID
 # Test_SteamID:76561199015522225 -> Sonrotz0815
 
+# # generate classes
+# test = Spielcards()
 
-# generate classes
-test = Spielcards()
+# startpage = Startpage()
 
-startpage = Startpage()
+# url,title=getSteamGamesbyID("76561199015522225")
 
+# # print(len(url), len(title) )
+# count=0
+# for s in url:
+#     if s == 0:
+#         url[count]="https://cdn-icons-png.flaticon.com/512/16/16096.png"
+#     count+=1
+# test.showallCards(url,title)
+# # test.showCard(url[0],title[0])
 
-
-
-url,title=getSteamGamesbyID("76561199015522225")
-
-# print(len(url), len(title) )
-count=0
-for s in url:
-    if s == 0:
-        url[count]="https://cdn-icons-png.flaticon.com/512/16/16096.png"
-    count+=1
-test.showallCards(url,title)
-# test.showCard(url[0],title[0])
