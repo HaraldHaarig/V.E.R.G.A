8537a80c0604ec6a09d6a49af815b5bbd9e64239
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2024-05-15 11:54:19 +0200

    SignIn and Login working & passwords now hashed

--- Tkinter_GUI/Login.py
+++ Tkinter_GUI/Login.py
@@ -1,6 +1,7 @@
 from customtkinter import *
 from PIL import Image
-from sympy import false
+from sympy import false, true
+import bcrypt
 
 class Login:
     def __init__(self):
@@ -11,10 +12,12 @@ class Login:
         side_img_data = Image.open("Tkinter_GUI/Images/Background_frfr.png")
         email_icon_data = Image.open("Tkinter_GUI/Images/Username-icon.png")
         password_icon_data = Image.open("Tkinter_GUI/Images/password-icon.png")
+        steam_icon_data=Image.open("Tkinter_GUI/Images/steamIcon.png")
 
         side_img = CTkImage(dark_image=side_img_data, light_image=side_img_data, size=(300, 480))
         email_icon = CTkImage(dark_image=email_icon_data, light_image=email_icon_data, size=(20,20))
         password_icon = CTkImage(dark_image=password_icon_data, light_image=password_icon_data, size=(17,17))
+        steam_icon=CTkImage(dark_image=steam_icon_data,light_image=steam_icon_data, size=(20,20))
 
         CTkLabel(master=self.app, text="", image=side_img).pack(expand=True, side="left")
 
@@ -34,39 +37,79 @@ class Login:
         
         self.password=CTkEntry(master=self.frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1, text_color="#000000", show="*")
         self.password.pack(anchor="w", padx=(25, 0))
+        
+        CTkLabel(master=self.frame, text="  (Only for SignIn) SteamID:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14), image=steam_icon, compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
+
+        self.steamId=CTkEntry(master=self.frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1, text_color="#000000")
+        self.steamId.pack(anchor="w", padx=(25,0))
 
         CTkButton(master=self.frame, text="Login", fg_color="#601E88", hover_color="#E44982", font=("Arial Bold", 12), text_color="#ffffff", width=225,command=self.pressedLogin).pack(anchor="w", pady=(40, 0), padx=(25, 0))
+        CTkButton(master=self.frame, text="Create V.E.R.G.A Account", fg_color="#601E88", hover_color="#E44982", font=("Arial Bold", 12), text_color="#ffffff", width=225, command=self.pressedSignIn).pack(anchor="w", pady=(20, 0), padx=(25, 0))
         self.app.mainloop()
+
+    def pressedSignIn(self):
+        username=self.username.get()
+        password=self.password.get()
+        steam=self.steamId.get()
+        salt=bcrypt.gensalt()
+        self.checkSignIn(username,password,salt,steam)
+
+    def checkSignIn(self,username,password,salt,steamid):
+        temp=password.encode('utf-8')
+        finalpassword=bcrypt.hashpw(temp,salt)
+        print("Enter SingIn")
+        exists=false
+        try:
+            db=open('db.txt','r')
+
+            for user in db:
+                if(username==user.split(";")[0]):
+                    exists=true
+                    break
+
+            if(exists):
+                print("Username already exists")
+            else:
+                db.close()
+                db=open('db.txt','a')
+                db.write(username+";"+str(finalpassword.decode("utf-8"))+";"+steamid+"\n")
+
+        except FileNotFoundError:
+            db=open('db.txt','w')
+
+
+
+
 
     def pressedLogin(self):
         username=self.username.get()
         password=self.password.get()
-        self.checkAccount(username,password)
+        self.checkLogin(username,password)
 
-    def checkAccount(self,username,password):
+
+
+    def checkLogin(self,username,password):
         print("Enter db")
-        found=0
+        isempty=true
         try:
             db=open('db.txt','r')
             
             for user in db:
+                isempty=false
                 curr=user.split(";")
-                if(curr[0]==username and curr[1].split("\n")[0]==password): #Voll Pfusch
-                    print("Login granted on Account: "+username +" with password: "+password)
-                    found=1
+                encoded=password.encode("utf-8")
+                hashed=bytes(curr[1],"utf-8")
+                
+                if(curr[0]==username and bcrypt.checkpw(encoded,hashed)): #Voll Pfusch
+                    print("Login granted on Account: "+username)
                     break
-                elif(curr[0]==username and curr[1].split("\n")[0]!=password):
-                    found=2
-                    break
-
-            if(found==0):
-                db.close()
-                db=open('db.txt','a')
-                db.write(username+";"+password+"\n")
-                print("Account Created with username: "+username+" and password: "+password)
-            elif(found==2):
-                print("Username already exists")
-
+                elif(curr[0]==username and not bcrypt.checkpw(encoded,hashed)):
+                    print("Username exists but password is wrong")
+                    break            
+                else:
+                    print("Your User does not exists. Pls go to Sign in")
+            if(isempty):
+                print("Empty db go to Sign In")
         except FileNotFoundError:
             db=open('db.txt','w')
             
