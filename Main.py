e40003e08427601ed7ed39844164c041e63d07d6
HaraldHaarig <132935818+HaraldHaarig@users.noreply.github.com>
2024-05-31 14:59:59 +0200

    Bibliothek working with Spielcards and Spielbeschreibung

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
+from zmq import NULL
 from Tkinter_GUI.Bibliothek import Bibliothek
 from Tkinter_GUI.Spielcards import Spielcards
 
 # generate classes
-bibliothek = Bibliothek()
+bibliothek = Bibliothek(False, None, None, None)
--- Tkinter_GUI/Bibliothek.py
+++ Tkinter_GUI/Bibliothek.py
@@ -4,14 +4,17 @@ import tkinter as tk
 from PIL import Image
 from ctypes import windll
 import ctypes
+
+from pydantic import PositiveInt
 from Tkinter_GUI.Spielcards import Spielcards
 from API.SteamAPI import getSteamGamesbyID
 
 
 
 class Bibliothek:
-    def __init__(self):
+    def __init__(self, bool, urls,titles,details):
         self.root_tk = CTk()
+        self.opened = bool
         # user32 = ctypes.windll.user32
         # screenwidth = user32.GetSystemMetrics(0)
         # screenheight=user32.GetSystemMetrics(1)
@@ -30,11 +33,6 @@ class Bibliothek:
         
         self.root_tk.title("Bibliothek")
 
-        #Pictures
-        leftside_img_data = Image.open("Tkinter_GUI/Images/Background_frfr.png")
-        background_img_data = Image.open("Tkinter_GUI/Images/Background_frfr.png")
-        #hier weiter machen
-
         #Right side of the libary
         self.mainframe= CTkFrame(self.root_tk, width=920,height=720, fg_color="#555555") #originalfarbe #222234
         self.mainframe.place(anchor=tk.E, relx=1,rely=0.5)
@@ -43,30 +41,46 @@ class Bibliothek:
         self.leftframe= CTkFrame(self.root_tk, width=340,height=720, fg_color="#ffffff") 
         self.leftframe.place(anchor=tk.W, relx=0.0,rely=0.5)
 
-        self.spielcards = Spielcards(self.mainframe)
+        self.spielcards = Spielcards(self.mainframe, self.root_tk)
 
-        url,title,details=getSteamGamesbyID("76561199015522225")
+        if self.opened == False:
+            print("test already opened false")
+            self.showCards()
+        else:
+            print("callShowallCards because true")
+            self.callshowallCards(urls,titles,details)
 
-        count=0
-        for s in url:
-            if s == 0:
-                url[count]="https://cdn-icons-png.flaticon.com/512/16/16096.png"
-            count+=1
-        self.spielcards.showallCards(url,title,details)
-        #self.spielcards.showCard(url[0],title[0], count)
+        # else:
+        #     self.callshowallCards()
+
+        # url,title,details=getSteamGamesbyID("76561199015522225")
+
+        # count=0
+        # for s in url:
+        #     if s == 0:
+        #         url[count]="https://cdn-icons-png.flaticon.com/512/16/16096.png"
+        #     count+=1
+        # self.spielcards.showallCards(url,title,details)
+        # self.spielcards.showCard(url[0],title[0], count)
 
         #self.showCards()
         
         self.root_tk.mainloop()
 
     def showCards(self):
-        print("hallo")
-        url,title=getSteamGamesbyID("76561199015522225")
+        print("showCards opened because bool is False")
+
+        url,title,details=getSteamGamesbyID("76561199015522225")
         count=0
         for s in url:
             if s == 0:
                 url[count]="https://cdn-icons-png.flaticon.com/512/16/16096.png"
-                count+=1
-        self.spielcards.showallCards(url,title, count)
+            count += 1
+        self.spielcards.showallCards(url,title,details)
+        #self.spielcards.showCard(url[0],title[0], count)
+    
+    def callshowallCards(self, urls,titles,details):
+        print("callshowallCards called becuase bool is True")
+        self.spielcards.showallCards(urls,titles,details)
 
         
