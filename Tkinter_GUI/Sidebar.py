2c4dfc3b5da56bd48148e2a6ec52e75f3ab65f30
binmarkoo <101064382+binmarkoo@users.noreply.github.com>
2024-06-05 21:02:41 +0200

    finished design of the Sidebar and adjusted some icons

--- Tkinter_GUI/Sidebar.py
+++ Tkinter_GUI/Sidebar.py
@@ -1,11 +1,14 @@
 import tkinter
 from customtkinter import *
 from PIL import Image
-from numpy import size
+from matplotlib.backend_bases import cursors
+from numpy import imag, size
+from pyparsing import col
 import pywinstyles
 
 class Sidebar:
     def __init__(self, app):
+        image_home = CTkImage(Image.open("Tkinter_GUI/StartseiteIMG/home.png"), size=(30, 30))
         image_geschenk = CTkImage(Image.open("Tkinter_GUI/StartseiteIMG/geschenk.png"), size=(30, 30))
         image_joystick = CTkImage(Image.open("Tkinter_GUI/StartseiteIMG/joystick.png"), size=(30, 30))
         image_notes = CTkImage(Image.open("Tkinter_GUI/StartseiteIMG/notes.png"), size=(30, 30))
@@ -26,49 +29,17 @@ class Sidebar:
                              image=image_icon)
         self.icon.place(x=150, y=100, anchor=tkinter.CENTER)
 
-        self.store_btn = CTkButton(master=self.sidebar,
-                                   image=image_shoppingcart,
-                                   text="Store",
-                                   width=100,
-                                   height=60,
-                                   fg_color="#000001",
-                                   cursor="hand2",
-                                   font=("Arial", 35))
-        pywinstyles.set_opacity(self.store_btn, color="#000001")
-        self.store_btn.place(x=150, y=220, anchor=tkinter.CENTER)
-
-        self.mygames_btn = CTkButton(master=self.sidebar,
-                                     image=image_joystick,
-                                     text="My Games",
-                                     width=100,
-                                     height=60,
-                                     fg_color="#000001",
-                                     cursor="hand2",
-                                     font=("Arial", 35))
-        pywinstyles.set_opacity(self.mygames_btn, color="#000001")
-        self.mygames_btn.place(x=150, y=310, anchor=tkinter.CENTER)
-
-        self.notes_btn = CTkButton(master=self.sidebar,
-                                   image=image_notes,
-                                   text="Notes",
-                                   width=100,
-                                   height=60,
-                                   fg_color="#000001",
-                                   cursor="hand2",
-                                   font=("Arial", 35))
-        pywinstyles.set_opacity(self.notes_btn, color="#000001")
-        self.notes_btn.place(x=150, y=400, anchor=tkinter.CENTER)
-
-        self.wishlist_btn = CTkButton(master=self.sidebar,
-                                      image=image_geschenk,
-                                      text="Wishlist",
-                                      width=100,
-                                      height=60,
-                                      fg_color="#000001",
-                                      cursor="hand2",
-                                      font=("Arial", 35))
-        pywinstyles.set_opacity(self.wishlist_btn, color="#000001")
-        self.wishlist_btn.place(x=150, y=490, anchor=tkinter.CENTER)
+        self.home_btn = CTkButton(master=self.sidebar,
+                                  image=image_home,
+                                  text="Home",
+                                  width=100,
+                                  height=60,
+                                  fg_color="#000001",
+                                  cursor="hand2",
+                                  font=("Arial", 35))
+        #self.home_btn.bind("<Button-1>", lambda e,:openHome())
+        pywinstyles.set_opacity(self.home_btn, color="#000001")
+        self.home_btn.place(x=150, y=220, anchor=tkinter.CENTER)
 
         self.profile_btn = CTkButton(master=self.sidebar,
                                      image=image_user,
@@ -79,4 +50,48 @@ class Sidebar:
                                      cursor="hand2",
                                      font=("Arial", 35))
         pywinstyles.set_opacity(self.profile_btn, color="#000001")
-        self.profile_btn.place(x=150, y=580, anchor=tkinter.CENTER)
+        self.profile_btn.place(x=150, y=310, anchor=tkinter.CENTER)
+
+        self.store_btn = CTkButton(master=self.sidebar,
+                                   image=image_shoppingcart,
+                                   text="Store",
+                                   width=100,
+                                   height=60,
+                                   fg_color="#000001",
+                                   cursor="hand2",
+                                   font=("Arial", 35))
+        pywinstyles.set_opacity(self.store_btn, color="#000001")
+        self.store_btn.place(x=150, y=400, anchor=tkinter.CENTER)
+
+        self.mygames_btn = CTkButton(master=self.sidebar,
+                                     image=image_joystick,
+                                     text="My Games",
+                                     width=100,
+                                     height=60,
+                                     fg_color="#000001",
+                                     cursor="hand2",
+                                     font=("Arial", 35))
+        pywinstyles.set_opacity(self.mygames_btn, color="#000001")
+        self.mygames_btn.place(x=150, y=490, anchor=tkinter.CENTER)
+
+        self.wishlist_btn = CTkButton(master=self.sidebar,
+                                      image=image_geschenk,
+                                      text="Wishlist",
+                                      width=100,
+                                      height=60,
+                                      fg_color="#000001",
+                                      cursor="hand2",
+                                      font=("Arial", 35))
+        pywinstyles.set_opacity(self.wishlist_btn, color="#000001")
+        self.wishlist_btn.place(x=150, y=580, anchor=tkinter.CENTER)
+
+        self.notes_btn = CTkButton(master=self.sidebar,
+                                   image=image_notes,
+                                   text="Notes",
+                                   width=100,
+                                   height=60,
+                                   fg_color="#000001",
+                                   cursor="hand2",
+                                   font=("Arial", 35))
+        pywinstyles.set_opacity(self.notes_btn, color="#000001")
+        self.notes_btn.place(x=150, y=670, anchor=tkinter.CENTER)
