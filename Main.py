d120ca20c04f91652357c94f5c8b3dfbafe40a74
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2024-06-06 18:41:12 +0200

    mini changes in main for merge

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@ from zmq import NULL
 # from Tkinter_GUI.Bibliothek import Bibliothek
 from Tkinter_GUI.Spielcards import Spielcards
 from Tkinter_GUI.Bibliothek import Bibliothek
-from GameStarter import startGame
+
 
 
 # generate classes
-# bibliothek = Bibliothek(False, None, None, None)
-startGame("Palworld")
+bibliothek = Bibliothek(False, None, None, None)
+
