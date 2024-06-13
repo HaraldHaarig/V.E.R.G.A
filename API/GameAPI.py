7f849cb0f4963c3ff6190c51b9496df734754564
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2024-06-13 08:40:17 +0200

    changes on GameAPI

--- API/GameAPI.py
+++ API/GameAPI.py
@@ -4,6 +4,35 @@ from dotenv import load_dotenv
 import os
 from zmq import NULL
 
+def getMoreDetailsALL(gamename):
+    load_dotenv(override=True)
+
+    rapid_key=os.getenv("RAPID_API_KEY")
+    raw_key=os.getenv("RAWG_API_KEY")
+
+    url = "https://rawg-video-games-database.p.rapidapi.com/games?key="+raw_key
+
+    headers = {
+        "X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com",
+        "X-RapidAPI-Key": rapid_key
+    }
+    url+="&search_exact=true&search_precise=true&search="+gamename
+    response = requests.get(url, headers=headers)
+    data=response.json()
+    
+    try:
+        if(data!=[] and data['results']!=[]):
+            all_Platforms=[]
+
+            for platforms in data['results'][0]['platforms']:
+                all_Platforms.append(platforms['platform']['name'])
+
+            return(data['results'][0]['released'], data['results'][0]['metacritic'], all_Platforms,data['results'][0]['description'],data['results'][0]['esrb_rating'][0]['name'])
+        else:
+            return NULL
+    except KeyError:
+        return NULL
+
 def getMoreDetails(gamename):
     
     load_dotenv(override=True)
@@ -82,6 +111,6 @@ def getAllGames(pages_anz):
             count+=1
             games.append(result['name'])
             spareImg.append(result['background_image'])
-            details.append(getMoreDetails(result['name']))
+            details.append(getMoreDetailsALL(result['name']))
     print(count)
     return games,spareImg,details
